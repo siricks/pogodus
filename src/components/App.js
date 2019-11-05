a38204bf7330edcc5ed2e4ca3c32f1ac9a29ddb7
Siricks <prokopyev_sergey@inbox.ru>
2019-11-05 15:01:43 +0300

    cards adaptive

--- src/components/App.js
+++ src/components/App.js
@@ -80,7 +80,7 @@ function App() {
 
     return (
         <div className="App">
-            <Aside/>
+            {/*<Aside/>*/}
             <div className="root__container">
                 <Header/>
                 <Search search={search}/>
