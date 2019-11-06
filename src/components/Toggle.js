41f18f28f50be1e8ecad5622073819f66e0a27e1
Siricks <prokopyev_sergey@inbox.ru>
2019-11-06 17:02:12 +0300

    add input

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import {func, string} from 'prop-types';
-
-
-import {ReactComponent as MoonIcon} from './icons/moon.svg';
-import {ReactComponent as SunIcon} from './icons/sun.svg';
 import ToggleContainer from './Toggle.styled';
 
 const Toggle = ({theme, toggleTheme}) => {
     const isLight = theme === 'light';
 
     return (
-        <ToggleContainer lightTheme={isLight} onClick={toggleTheme}>
-            <SunIcon/>
-            <MoonIcon/>
-        </ToggleContainer>
+        <ToggleContainer lightTheme={isLight} toggleTheme={toggleTheme}/>
     )
 }
 
--- src/components/Toggle.styled.js
+++ src/components/Toggle.styled.js
@@ -1,35 +1,23 @@
 import styled from "styled-components";
+import React from 'react';
 
-const ToggleContainer = styled.button`
-  background: ${({ theme }) => theme.gradient};
-  border: 2px solid ${({ theme }) => theme.toggleBorder};
-  border-radius: 30px;
-  cursor: pointer;
-  display: flex;
-  font-size: 0.5rem;
-  justify-content: space-between;
-  margin: 0 auto;
-  overflow: hidden;
-  padding: 0.5rem;
-  position: relative;
-  width: 8rem;
-  height: 4rem;
-
-  svg {
-    height: auto;
-    width: 2.5rem;
-    transition: all 0.3s linear;
-    
-    // sun icon
-    &:first-child {
-      transform: ${({ lightTheme }) => lightTheme ? 'translateY(0)' : 'translateY(100px)'};
-    }
-    
-    // moon icon
-    &:nth-child(2) {
-      transform: ${({ lightTheme }) => lightTheme ? 'translateY(-100px)' : 'translateY(0)'};
-    }
-  }
+const HiddenCheckbox = styled.input.attrs({type: 'checkbox'})`
 `;
+
+const ToggleContainer = ({lightTheme, toggleTheme}) => (
+    <>
+        <span className="mode-toggle__text">Light</span>
+        <label className="toggle-button">
+            <HiddenCheckbox className="toggle-button__input" onClick={toggleTheme}/>
+            <span className={`toggle-button__bg ${
+                lightTheme ? `` : 'toggle-button__bg-checked'
+                }`}></span>
+            <span className={`toggle-button__circle ${
+                lightTheme ? `` : 'toggle-button__circle-checked'
+                }`}></span>
+        </label>
+        <span className="mode-toggle__text">Dark</span>
+    </>
+);
 
 export default ToggleContainer;
