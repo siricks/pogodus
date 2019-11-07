2ef37e114854a37333006ec652449284702b517d
Siricks <prokopyev_sergey@inbox.ru>
2019-11-07 21:27:30 +0300

    Updates

---
+++ precache-manifest.e106827df968060faef4821869e49794.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c91c99a4102e45a61f7695e9acbfe7",
+    "url": "/pogodus/index.html"
+  },
+  {
+    "revision": "c478247ab82bd4a25aaa",
+    "url": "/pogodus/static/css/main.b8504a27.chunk.css"
+  },
+  {
+    "revision": "aa724ee819b59a255576",
+    "url": "/pogodus/static/js/2.cab370ba.chunk.js"
+  },
+  {
+    "revision": "c478247ab82bd4a25aaa",
+    "url": "/pogodus/static/js/main.e9d2b183.chunk.js"
+  },
+  {
+    "revision": "cf17b9ebb9b745db6a5f",
+    "url": "/pogodus/static/js/runtime-main.231825e8.js"
+  },
+  {
+    "revision": "857ad6cd01a4016d6601ed9aa90c4ca7",
+    "url": "/pogodus/static/media/AddIcon.857ad6cd.svg"
+  },
+  {
+    "revision": "82758ac102df6c4d71cfb1fb6dfcc36c",
+    "url": "/pogodus/static/media/CityIllustration.82758ac1.svg"
+  },
+  {
+    "revision": "6d6a14edb9dba4152a83d9b561d3d988",
+    "url": "/pogodus/static/media/Clouds.6d6a14ed.svg"
+  },
+  {
+    "revision": "919b446ccb6880f13167dd83d229bac0",
+    "url": "/pogodus/static/media/Fog.919b446c.svg"
+  },
+  {
+    "revision": "8fa5f2dde95cfc61609833a074d7cf04",
+    "url": "/pogodus/static/media/Haze.8fa5f2dd.svg"
+  },
+  {
+    "revision": "b44b59cc936a8e66a17ac5615fe1ba29",
+    "url": "/pogodus/static/media/Rain.b44b59cc.svg"
+  },
+  {
+    "revision": "934895c161075fae3a33b80c275aeefb",
+    "url": "/pogodus/static/media/Storm.934895c1.svg"
+  },
+  {
+    "revision": "5ae379e33a3a0a0301561e72cb517abf",
+    "url": "/pogodus/static/media/Sunny.5ae379e3.svg"
+  },
+  {
+    "revision": "17fad0b703a9bf1ac461064413556558",
+    "url": "/pogodus/static/media/logo.17fad0b7.svg"
+  },
+  {
+    "revision": "3fb4ffc809ff1a45f635eac7d9e1143e",
+    "url": "/pogodus/static/media/menu.3fb4ffc8.svg"
+  },
+  {
+    "revision": "c6a0effbe867db0941bd5592f8f43810",
+    "url": "/pogodus/static/media/search.c6a0effb.svg"
+  }
+]);
